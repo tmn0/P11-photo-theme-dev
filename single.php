0e809c241cfc0a9a53eaedccb299b7687517be92
tmn0 <attmn0@gmail.com>
2023-10-11 16:57:54 +0200

    Stage 2e ter
    WIP Single, Taxo ok

--- single.php
+++ single.php
@@ -3,23 +3,60 @@
 <?php get_template_part('template-part/navmenu'); ?>
 </header>
 
-<?php 
-while (have_posts()) :
-    the_post();
-?>
+<section id="single-content">
 
-    <div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-        <h1><?php the_title(); ?></h1>
-        <div class="post-content">
-            <?php the_content(); ?>
+    <div class="single-content-left">
+        <?php 
+        while (have_posts()) :
+            the_post();
+        ?>
+
+        <div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+             <h1><?php echo strtoupper(get_the_title()); ?></h1>
         </div>
-        
-        
-        
     </div>
 
-<?php
-endwhile;
+
+        <?php
+    // Custom taxo 1
+    $terms = get_the_terms(get_the_ID(), 'categorie');
+    // Custom taxo 2
+    $terms_format = get_the_terms(get_the_ID(), 'format');
+
+    // Call custom taxos
+    if (($terms || $terms_format) && (!is_wp_error($terms) || !is_wp_error($terms_format))) {
+        echo '<div>'; 
+
+        if ($terms) {
+            echo '<h2>Categorie:</h2>';
+            foreach ($terms as $term) {
+                echo '<p>' . $term->name . '</p>';
+            }
+        }
+
+        if ($terms_format) {
+            echo '<h2>Format:</h2>';
+            foreach ($terms_format as $term) {
+                echo '<p>' . $term->name . '</p>';
+            }
+        }
+
+        echo '</div>'; 
+    }
 ?>
+
+    
+
+
+
+    <div class="single-content-right">        
+        <?php the_content(); ?>                
+    </div>
+
+        <?php
+        endwhile;
+        ?>
+
+</section>
 
 <?php get_footer(); ?>
--- template-part/hero.php
+++ template-part/hero.php
@@ -6,12 +6,13 @@
 <!-- selectable hero img in wordpress -->    
     
         <div id="custom-hero-image">
-        <?php if (get_header_image()) : ?>
-            <img src="<?php header_image(); ?>" alt="<?php echo esc_attr(get_bloginfo('name', 'display')); ?>">
-           
-        <?php else: ?>
-        <img src="<?php echo get_template_directory_uri() . '/medias/hero-bg.jpeg'?>"> 
+            <?php if (get_header_image()) : ?>
+                <img src="<?php header_image(); ?>" alt="<?php echo esc_attr(get_bloginfo('name', 'display')); ?>">
+            
+            <?php else: ?>
+            <img src="<?php echo get_template_directory_uri() . '/medias/hero-bg.jpeg'?>"> 
+            <?php endif ?>  
         </div>
-        <?php endif ?>  
+        
 </div>
 
